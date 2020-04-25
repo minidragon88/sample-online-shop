91932b070f9db693ac555b423d4cca68f82bcb9a
Phu Nguyen <phu@treasure-data.com>
2020-04-26 03:59:07 +0700

    Update checkstyle

--- src/main/java/com/phu/onlineshop/OnlineShopApp.java
+++ src/main/java/com/phu/onlineshop/OnlineShopApp.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
 public class OnlineShopApp
 {
-    private OnlineShopApp()
-    {}
-
     public static void main(final String[] args)
     {
         SpringApplication.run(OnlineShopApp.class, args);
